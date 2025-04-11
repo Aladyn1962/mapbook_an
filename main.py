dbb76ee367664fc4c327186d09e3ca3b63812ad3
Aladyn1962 <mareknowak1238@gmail.com>
2025-04-11 09:32:49 +0200

    upgraded

---
+++ main.py
@@ -0,0 +1,10 @@
+users:list = [
+    {'name':"bernard",'location':'Ełk','posts':400},
+    {'name':"krzysztof",'location':'Białobrzegi','posts':500},
+    {'name':"maja",'location':'Świecie','posts':300},
+    {'name':"zuzanna",'location':'Radzyń_Podlaski','posts':700},
+]
+
+
+        print(f'twój znajomy {user["name"]} z miejscowości {user["location"]}opublikował {user["posts"]}postów')
+get_user_info(users)
